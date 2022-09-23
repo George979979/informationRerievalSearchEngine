d7de532873b8d6dab8c1f693845e5ec078398244
eustacez1 <yz751@georgetown.edu>
2022-09-23 02:26:37 -0400

    Create testReadFiles.py

---
+++ testReadFiles.py
@@ -0,0 +1,40 @@
+import re
+
+# testing block
+#####################################################
+# test string methods
+
+# string = "<!-- PJG FTAG 4700 -->"
+# re.split('[a-f]+', '0a3B9',flags=re.IGNORECASE)
+# print(string.startswith("<!--") and string.endswith("-->"))
+
+
+testFile = open("test.tex")
+lines = testFile.read().splitlines()
+testFile.close()
+lines = [i for i in lines if not isInvalidLine(i)] # comment line
+
+print(lines)
+
+
+
+
+
+# Output
+#####################################################
+def isInvalidLine(s):
+    
+    # comments 
+    if(s.startswith("<!--") and s.endswith("-->")): return True 
+    
+    # empty str
+    if(not s):  return True
+    
+    # str of spaces
+    if(s.isspace()): return True   
+    
+    # tags
+    if(s in ['<DOC>','</DOC>','<TEXT>','</TEXT>']): return True
+    
+    # invalid line
+    if(s.startswith('<PARENT>') and s.endswith('</PARENT>')): return True   
